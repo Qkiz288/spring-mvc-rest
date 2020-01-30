d5655b4f642d6fdb8336f5dfb68a6b1f301e7661
Qkiz288 <qkiz288@gmail.com>
2020-01-30 15:44:46 +0100

    [387] Implemented CategoryController

--- src/main/java/com/kkukielka/services/CategoryService.java
+++ src/main/java/com/kkukielka/services/CategoryService.java
@@ -1,11 +1,9 @@
 package com.kkukielka.services;
 
 import com.kkukielka.api.v1.model.CategoryDTO;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface CategoryService {
 
     List<CategoryDTO> getAllCategories();
--- src/main/java/com/kkukielka/services/CategoryServiceImpl.java
+++ src/main/java/com/kkukielka/services/CategoryServiceImpl.java
@@ -3,10 +3,12 @@ package com.kkukielka.services;
 import com.kkukielka.api.v1.mapper.CategoryMapper;
 import com.kkukielka.api.v1.model.CategoryDTO;
 import com.kkukielka.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryMapper categoryMapper;
