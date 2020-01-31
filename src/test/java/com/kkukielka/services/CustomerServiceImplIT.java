a75d78bfc5234f22a23a13846f2ad0d6c5338c91
Qkiz288 <kkukielka@griddynamics.com>
2020-01-31 15:57:44 +0100

    [393] Integration test with DataJpaTest for PATCH

---
+++ src/test/java/com/kkukielka/services/CustomerServiceImplIT.java
@@ -0,0 +1,107 @@
+package com.kkukielka.services;
+
+import com.kkukielka.api.v1.mapper.CustomerMapper;
+import com.kkukielka.api.v1.model.CustomerDTO;
+import com.kkukielka.bootstrap.Bootstrap;
+import com.kkukielka.domain.Customer;
+import com.kkukielka.repositories.CategoryRepository;
+import com.kkukielka.repositories.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
+
+@Slf4j
+@DataJpaTest
+@RunWith(SpringRunner.class)
+public class CustomerServiceImplIT {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    private CustomerService customerService;
+
+    @Before
+    public void setUp() {
+        log.debug("Loading customer data");
+        log.debug("Count: " + customerRepository.findAll().size());
+
+        Bootstrap bootstrap = new Bootstrap(categoryRepository, customerRepository);
+        bootstrap.run();
+
+        customerService = new CustomerServiceImpl(customerRepository, CustomerMapper.INSTANCE);
+    }
+
+    @Test
+    public void patchCustomerUpdateFirstName() {
+        String updatedName = "UpdatedName";
+        long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.getOne(id);
+        assertNotNull(originalCustomer);
+        //save original first name
+        String originalFirstName = originalCustomer.getFirstname();
+        String originalLastName = originalCustomer.getLastname();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setFirstname(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        Customer updatedCustomer = customerRepository.findById(id).get();
+
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getFirstname());
+        assertThat(originalFirstName, not(equalTo(updatedCustomer.getFirstname())));
+        assertThat(originalLastName, equalTo(updatedCustomer.getLastname()));
+    }
+
+    @Test
+    public void patchCustomerUpdateLastName() {
+        String updatedName = "UpdatedName";
+        long id = getCustomerIdValue();
+
+        Customer originalCustomer = customerRepository.getOne(id);
+        assertNotNull(originalCustomer);
+
+        //save original first/last name
+        String originalFirstName = originalCustomer.getFirstname();
+        String originalLastName = originalCustomer.getLastname();
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setLastname(updatedName);
+
+        customerService.patchCustomer(id, customerDTO);
+
+        Customer updatedCustomer = customerRepository.findById(id).get();
+
+        assertNotNull(updatedCustomer);
+        assertEquals(updatedName, updatedCustomer.getLastname());
+        assertThat(originalFirstName, equalTo(updatedCustomer.getFirstname()));
+        assertThat(originalLastName, not(equalTo(updatedCustomer.getLastname())));
+    }
+
+    private Long getCustomerIdValue(){
+        List<Customer> customers = customerRepository.findAll();
+
+        System.out.println("Customers Found: " + customers.size());
+
+        //return first id
+        return customers.get(0).getId();
+    }
+
+}
