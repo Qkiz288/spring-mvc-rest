727676617e50fb27b42fec7c1d589d5795427afd
Qkiz288 <kkukielka@griddynamics.com>
2020-02-01 15:52:22 +0100

    [398] Refactored Controllers to use RestController annotation

--- src/main/java/com/kkukielka/controllers/v1/CategoryController.java
+++ src/main/java/com/kkukielka/controllers/v1/CategoryController.java
@@ -2,16 +2,11 @@ package com.kkukielka.controllers.v1;
 
 import com.kkukielka.api.v1.model.CategoryDTO;
 import com.kkukielka.api.v1.model.CategoryListDTO;
-import com.kkukielka.domain.Category;
 import com.kkukielka.services.CategoryService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping(CategoryController.BASE_URL)
 public class CategoryController {
 
@@ -24,15 +19,15 @@ public class CategoryController {
     }
 
     @GetMapping
-    public ResponseEntity<CategoryListDTO> getAllCategories() {
-        return new ResponseEntity<>(
-                new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryListDTO getAllCategories() {
+        return new CategoryListDTO(categoryService.getAllCategories());
     }
 
     @GetMapping("/{name}")
-    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
-        return new ResponseEntity<>(
-                categoryService.getCategoryByName(name), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName(@PathVariable String name) {
+        return categoryService.getCategoryByName(name);
 
     }
 }
